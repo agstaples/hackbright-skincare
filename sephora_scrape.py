b8c9b840a10f3e0c7219b88f272dd8ac1ab903f7
vagrant <vagrant@vagrant.vm>
2018-07-26 19:38:51 +0000

    sephora page scrape tested

--- sephora_scrape.py
+++ sephora_scrape.py
@@ -43,14 +43,15 @@ def make_sephora_page_soup():
     split_urls = divide_url_list()
     print(len(split_urls))
     valid_skincare_urls = []
-    counter = 0
+    counter = 103
 
-    with open("valid_skincare_urls.txt", "w") as valid_skin_urls:
-        for urls in split_urls:
+    with open("valid_skin_urls.txt", "w+") as valid_skin_urls:
+        for urls in split_urls[104:]:
             counter += 1
             print(counter)
             for url in urls:
                 url = str(url)
+                print(url)
                 # providing headers for client, otherwise request fails
                 # http = urllib3.PoolManager()
                 header = {"User-Agent":"Firefox"}
@@ -71,56 +72,66 @@ def make_sephora_page_soup():
                     else:
                         category_1 = categories_1_2[0].string
                         if category_1 == "Skincare":
-                            valid_skincare_urls.append(url)
-                            print(url)
                             valid_skin_urls.write(url+"\n")
+                            print("success")
+                            valid_skincare_urls.append(url)
 
-    return len(valid_skincare_urls), len(faulty_urls), len(valid_nonskincare_urls)
+    return len(valid_skincare_urls)
 
-make_sephora_page_soup()
+def scrape_relevant_product_info(doc="valid_skin_urls.txt"):
+    """Reads valid skincare product urls from external file and scrapes for relevant data"""
 
-# def scrape_relevant_product_info(doc="valid_skincare_urls.txt"):
-#     """Reads valid skincare product urls from external file and scrapes for relevant data"""
-
-#     with open(doc) as valid_skincare_urls:
-#         urls = valid_skincare_urls.readlines()
+    with open(doc) as valid_skin_urls:
+        urls = valid_skin_urls.readlines()
     
-#     for url in urls:
-#         url = str(url)
-#         # providing headers for client, otherwise request fails
-#         http = urllib3.PoolManager()
-#         header = {"User-Agent": "Human"}
-#         request = requests.get(url, headers=header)
-#         page = request.text
-#         soup = BeautifulSoup(page, "html.parser")
-#         product_name = soup.find(attrs={"class": "css-1g2jq23"})
-#         categories_1_2 = soup.find_all(attrs={"class": "css-u2mtre"})
-#         category_1 = category_1 = categories_1_2[0].string
-#         if len(categories_1_2) > 1:
-#             category_2 = categories_1_2[1].string
-#         else:
-#             category_2 = None
-#         category_3 = category_3 = soup.find(attrs={"class": "css-j60h5s"}).string
-#         # getting brand name
-#         brand = soup.find(attrs={"class": "css-cjz2sh"}).string
-#         # getting star rating as percentage
-#         stars_object = soup.find(attrs={"class": "css-dtomnp"})
-#         stars = float(stars_object["style"].strip("%").split(":")[-1])
-#         # getting price from sephora page
-#         price_string = soup.find(attrs={"class": "css-18suhml"}).string
-#         if price_string == None:
-#             price = 0
-#         else:
-#             price = float(price_string.strip("$"))
-    
+        for url in urls[:5]:
+            url = str(url.rstrip())
+            # providing headers for client, otherwise request fails
+            header = {"User-Agent":"Firefox"}
+            request = Request(url, headers=header)
+            page = urlopen(request)
+            soup = BeautifulSoup(page, "html.parser")
+            name = soup.find(attrs={"class": "css-1g2jq23"}).string
+            categories_1_2 = soup.find_all(attrs={"class": "css-u2mtre"})
+            if len(categories_1_2) == 0:
+                category_1 = soup.find(attrs={"class": "css-j60h5s"}).string
+            else:
+                category_1 = categories_1_2[0].string
+            if len(categories_1_2) > 1:
+                category_2 = categories_1_2[1].string
+            else:
+                category_2 = None
+            category_3 = category_3 = soup.find(attrs={"class": "css-j60h5s"}).string
+            # getting brand name
+            brand = soup.find(attrs={"class": "css-cjz2sh"}).string
+            # getting star rating as percentage
+            stars_object = soup.find(attrs={"class": "css-dtomnp"})
+            stars = float(stars_object["style"].strip("%").split(":")[-1])
+            # getting price from sephora page
+            price_string = soup.find(attrs={"class": "css-18suhml"}).string
+            if price_string == None:
+                price = 0
+            else:
+                price = float(price_string.strip("$"))
+            product_box = soup.find_all(attrs={'class': 'css-1juot2r'})
+            ingredients_all = product_box[-1].text
+            ingredients_list = ingredients_all.split("\n")
+            if len(ingredients_list[-1]) < 1:
+                ingredients = ingredients_list[-2].rstrip(".")
+            else:
+                ingredients = ingredients_list[-1].rstrip(".")
+
+
+
+
+scrape_relevant_product_info()  
 
 
 
 
 
 
-        # category1 = soup.find()
-        # # product_box = soup.find_all(attrs={'class': 'css-1juot2r'})
+
         # product_box = soup.find_all(attrs={'class': 'css-1juot2r'})
         # print(product_box)
 
