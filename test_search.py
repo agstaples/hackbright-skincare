35be648e9578a37e85d551925e7a895e993caa49
vagrant <vagrant@vagrant.vm>
2018-08-15 18:45:15 +0000

    setting up unittesting doc

---
+++ test_search.py
@@ -0,0 +1,42 @@
+
+
+from search import search_by_term, return_close_ing_matches
+import unittest
+
+
+
+
+class SkincareSearchUnitTestCase(unittest.TestCase):
+    """Class of tests to test functions in search.py"""
+
+
+    def test_search_by_term_1(self):
+        """tests search_by_term function"""
+
+        assert search_by_term(input) == output
+
+
+    def test_search_by_term_2(self):
+        """tests search_by_term function"""
+
+        assert search_by_term(input) == output
+
+
+    def test_search_by_term_3(self):
+        """tests search_by_term function"""
+
+        assert search_by_term(input) == output
+
+
+    def test_search_by_term_4(self):
+        """tests search_by_term function"""
+
+        assert search_by_term(input) == output
+
+
+
+
+if __name__ = "__main__":
+    unittest.main()
+
+
